0514a733fd9f6a9b270e2a00773a3bfe0d726ff8
bleubreeymeow <alris.dai2003@gmail.com>
2024-09-15 18:16:45 +0800

    changed to 2D arrays

--- lab1/lab1_p1.c
+++ lab1/lab1_p1.c
@@ -14,106 +14,107 @@ int uz;
 int atom_num;
 double a;
 
-void file_writing(char *lattice_structure, double *arr, int num){
+void file_writing(char *lattice_structure, double **arr, int num){
     char lattice_filename[50];
-    snprintf(lattice_filename, sizeof(lattice_filename), "lab1_%s.txt", lattice_structure);
+    snprintf(lattice_filename, sizeof(lattice_filename), "lab1_%s.xyz", lattice_structure);
 
     FILE *filepointer = fopen(lattice_filename, "w");
     
-    //fprintf(filepointer,"%d \n \n",(num));
+    fprintf(filepointer,"%d \n \n",(num));
 
     for (int i = 0; i < num ; i++){
-        //fprintf(filepointer,"%s \t %lf \t %lf \t %lf \n", ELEMENT_STR, arr[3 * i], arr[3 * i + 1] , arr[3 * i + 2]);
-        fprintf(filepointer,"%lf \t %lf \t %lf \n", arr[3 * i], arr[3 * i + 1] , arr[3 * i + 2]);
+        fprintf(filepointer,"%s \t %lf \t %lf \t %lf \n", ELEMENT_STR, arr[i][0], arr[i][1] , arr[i][2]);
+        //MATLAB fprintf(filepointer,"%lf \t %lf \t %lf \n",arr[i][0], arr[i][1] , arr[i][2]);
     }
 
     fclose(filepointer);
 }
 
-void fn_simplecubic(double *arr) {
+void fn_simplecubic(double **arr) {
     for (int i = 0; i < atom_num ; i++) {
-        arr[3 * i] = (i % ux) * a;
-        arr[3 * i + 1] = (i / ux) % uy * a;
-        arr[3 * i + 2] = (i / ux / uy) % uz * a;
-
+        arr[i][0] = (i % ux) * a;
+        arr[i][1] = (i / ux) % uy * a;
+        arr[i][2] = (i / ux / uy) % uz * a;
     }
     return;
 }
 
-void fn_bcc(double *arr){
-    for (int i = 0; i < atom_num ; i++) {
-        arr[6 * i] = (i % (ux)) * (a) ;
-        arr[6 * i + 1] = (i / ux) % uy * a;
-        arr[6 * i + 2] = (i / ux / uy) % uz * a;
-
-        arr[6 * i + 3] = arr[6 * i] + a * 0.5;
-        arr[6 * i + 4] = arr[6 * i + 1] + a * 0.5;
-        arr[6 * i + 5] = arr[6 * i + 2] + a * 0.5;
-    }
-    return;
-}
-
-void fn_fcc(double *arr){
-    for (int i = 0; i < atom_num ; i++) {
-        arr[12 * i] = (i % (ux)) * (a) ;
-        arr[12 * i + 1] = (i / ux) % uy * a;
-        arr[12 * i + 2] = (i / ux / uy) % uz * a;
-
-        arr[12 * i + 3] = arr[12 * i] + a * 0.5;
-        arr[12 * i + 4] = arr[12 * i + 1] + a * 0.5;
-        arr[12 * i + 5] = arr[12 * i + 2];
-
-        arr[12 * i + 6] = arr[12 * i] + a * 0.5;
-        arr[12 * i + 7] = arr[12 * i + 1];
-        arr[12 * i + 8] = arr[12 * i + 2] + a * 0.5;
-
-        arr[12 * i + 9] = arr[12 * i];
-        arr[12 * i + 10] = arr[12 * i + 1] + a * 0.5;
-        arr[12 * i + 11] = arr[12 * i + 2] + a * 0.5;
-    }
-    return;
-}
-
-void fn_diamond_fcc(double *arr){
-    for (int i = 0; i < atom_num ; i++) {
+void fn_bcc(double **arr){
+    for (int i = 0; i < atom_num * 2 ; i = i + 2) {
         //1st basis atom
-        arr[24 * i] = (i % (ux)) * (a) ;
-        arr[24 * i + 1] = (i / ux) % uy * a;
-        arr[24 * i + 2] = (i / ux / uy) % uz * a;
-        //(1/4 , 1/4 , 1/4) relative to 1st fcc basis atom
-        arr[24 * i + 12] = arr[24 * i] + a * 0.25;
-        arr[24 * i + 13] = arr[24 * i + 1] + a * 0.25;
-        arr[24 * i + 14] = arr[24 * i + 2] + a * 0.25;
+        arr[i][0] = (i % ux) * a;
+        arr[i][1] = (i / ux) % uy * a;
+        arr[i][2] = (i / ux / uy) % uz * a;
+        //2nd basis atom at (a/2, a/2, a/2) relative to 1st basis atom
+        arr[i + 1][0] = arr[i][0] + a * 0.5;
+        arr[i + 1][1] = arr[i][1] + a * 0.5;
+        arr[i + 1][2] = arr[i][2] + a * 0.5;
+    }
+    return;
+}
+
+void fn_fcc(double **arr){
+    for (int i = 0; i < atom_num * 4 ; i = i + 4) {
+        //1st basis atom
+        arr[i][0] = (i % ux) * a;
+        arr[i][1] = (i / ux) % uy * a;
+        arr[i][2] = (i / ux / uy) % uz * a;
+        //2nd basis atom at (a/2 , a/2 , 0) relative to 1st basis atom
+        arr[i + 1][0] = arr[i][0] + a * 0.5;
+        arr[i + 1][1] = arr[i][1] + a * 0.5;
+        arr[i + 1][2] = arr[i][2];
+        //3rd basis atom at (a/2 , 0 , a/2) relative to 1st basis atom       
+        arr[i + 2][0] = arr[i][0] + a * 0.5;
+        arr[i + 2][1] = arr[i][1];
+        arr[i + 2][2] = arr[i][2] + a * 0.5;
+        //4th basis atom at (0 , a/2 , a/2) relative to 1st basis atom
+        arr[i + 3][0] = arr[i][0];
+        arr[i + 3][1] = arr[i][1] + a * 0.5;
+        arr[i + 3][2] = arr[i][2] + a * 0.5;
+    }
+    return;
+}
+
+void fn_diamond_fcc(double **arr){
+    for (int i = 0; i < atom_num * 8 ; i = i + 8) {
+        //1st basis atom
+        arr[i][0] = (i % ux) * a;
+        arr[i][1] = (i / ux) % uy * a;
+        arr[i][2] = (i / ux / uy) % uz * a;
+        //(a/4 , a/4 , a/4) relative to 1st fcc basis atom
+        arr[i + 1][0] = arr[i][0] + a * 0.25;
+        arr[i + 1][1] = arr[i][1] + a * 0.25;
+        arr[i + 1][2] = arr[i][2] + a * 0.25;
 
         //================
-        //2nd basis atom at (1/2,1/2,0) from 1st basis atom
-        arr[24 * i + 3] = arr[24 * i] + a * 0.5;
-        arr[24 * i + 4] = arr[24 * i + 1] + a * 0.5;
-        arr[24 * i + 5] = arr[24 * i + 2];
-        //(1/4 , 1/4 , 1/4) relative to 2nd fcc basis atom
-        arr[24 * i + 15] = arr[24 * i + 3] + a * 0.25;
-        arr[24 * i + 16] = arr[24 * i + 4] + a * 0.25;
-        arr[24 * i + 17] = arr[24 * i + 5] + a * 0.25;
+        //2nd basis atom at (a/2, a/2, 0) from 1st basis atom
+        arr[i + 2][0] = arr[i][0] + a * 0.5;
+        arr[i + 2][1] = arr[i][1] + a * 0.5;
+        arr[i + 2][2] = arr[i][2];
+        //(a/4 , a/4 , a/4) relative to 2nd fcc basis atom
+        arr[i + 3][0] = arr[i + 2][0] + a * 0.25;
+        arr[i + 3][1] = arr[i + 2][1] + a * 0.25;
+        arr[i + 3][2] = arr[i + 2][2] + a * 0.25;
 
         //===================
-        //3rd basis atom at (1/2,0,1/2) from 1st basis atom
-        arr[24 * i + 6] = arr[24 * i] + a * 0.5;
-        arr[24 * i + 7] = arr[24 * i + 1];
-        arr[24 * i + 8] = arr[24 * i + 2] + a * 0.5;
-        //(1/4 , 1/4 , 1/4) relative to 3rd fcc basis atom
-        arr[24 * i + 18] = arr[24 * i + 6] + a * 0.25;
-        arr[24 * i + 19] = arr[24 * i + 7] + a * 0.25;
-        arr[24 * i + 20] = arr[24 * i + 8] + a * 0.25;
+        //3rd basis atom at (a/2, 0, a/2) from 1st basis atom
+        arr[i + 4][0] = arr[i][0] + a * 0.5;
+        arr[i + 4][1] = arr[i][1];
+        arr[i + 4][2] = arr[i][2] + a * 0.5;
+        //(a/4 , a/4 , a/4) relative to 3rd fcc basis atom
+        arr[i + 5][0] = arr[i + 4][0] + a * 0.25;
+        arr[i + 5][1] = arr[i + 4][1] + a * 0.25;
+        arr[i + 5][2] = arr[i + 4][2] + a * 0.25;
 
         //====================
-        //4th basis atom at (0,1/2,1/2) from 1st basis atom
-        arr[24 * i + 9] = arr[24 * i];
-        arr[24 * i + 10] = arr[24 * i + 1] + a * 0.5;
-        arr[24 * i + 11] = arr[24 * i + 2] + a * 0.5;
-        //(1/4 , 1/4 , 1/4) relative to 4th fcc basis atom
-        arr[24 * i + 21] = arr[24 * i + 9] + a * 0.25;
-        arr[24 * i + 22] = arr[24 * i + 10] + a * 0.25;
-        arr[24 * i + 23] = arr[24 * i + 11] + a * 0.25;
+        //4th basis atom at (0, a/2, a/2) from 1st basis atom
+        arr[i + 6][0] = arr[i][0];
+        arr[i + 6][1] = arr[i][1] + a * 0.5;
+        arr[i + 6][2] = arr[i][2] + a * 0.5;
+        //(a/4 , a/4 , a/4) relative to 4th fcc basis atom
+        arr[i + 7][0] = arr[i + 6][0] + a * 0.25;
+        arr[i + 7][1] = arr[i + 6][1] + a * 0.25;
+        arr[i + 7][2] = arr[i + 6][2] + a * 0.25;
     }
     return;
 }
@@ -134,10 +135,29 @@ int main(){
 
     atom_num = ux * uy * uz;
 
-    double *sc_arr = (double *)malloc(atom_num * 3 * sizeof(double));
-    double *bcc_arr = (double *)malloc(atom_num * 6 * sizeof(double));
-    double *fcc_arr = (double *)malloc(atom_num * 12 * sizeof(double));
-    double *diamond_fcc_arr = (double *)malloc(atom_num * 24 * sizeof(double));
+    //initialise 2D array for sc lattice
+    double **sc_arr = (double **)malloc(atom_num * sizeof(double *));
+    for (int i = 0; i < atom_num ; i++){
+        sc_arr[i] = (double *)malloc(3 * sizeof(double));
+    }
+
+    //initialise 2D array for bcc lattice
+    double **bcc_arr = (double **)malloc(atom_num * 2 * sizeof(double *));
+    for (int j = 0; j < atom_num * 2 ; j++){
+        bcc_arr[j] = (double *)malloc(3 * sizeof(double));
+    }
+
+    //initialise 2D array for fcc lattice
+    double **fcc_arr = (double **)malloc(atom_num * 4 * sizeof(double *));
+    for (int k = 0; k < atom_num * 4 ; k++){
+        fcc_arr[k] = (double *)malloc(3 * sizeof(double));
+    }
+
+    //initialise 2D array for diamond lattice
+    double **diamond_fcc_arr = (double **)malloc(atom_num * 8 * sizeof(double *));
+    for (int m = 0; m < atom_num * 8 ; m++){
+        diamond_fcc_arr[m] = (double *)malloc(3 * sizeof(double));
+    }
 
     fn_simplecubic(sc_arr);
     file_writing(SC_STR, sc_arr,atom_num);
